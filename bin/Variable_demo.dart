2fa92baf258daa4efe833e7ff9bd2e9def75ff2a
GOHULRAJ <gohulrajs71@gmail.com>
2024-09-10 22:55:05 +0530

    variable_demo added

---
+++ bin/Variable_demo.dart
@@ -0,0 +1,32 @@
+void main(){
+  print("Hello world!");
+  print("Sum of 2+3: ${2+3}");//
+  print("""
+  print
+  multiline 
+  string
+  """);
+  var name='Subhani';
+  print("Name: $name");//string
+  var num=39;
+  print("num: $num");//int
+  var weight=59.8;
+  print("weight: $weight"); //double
+  num=45;
+  print("reintialization-> num: $num");//reintialization
+
+   final int num1=32;
+   print("Static-> num1: $num1");//static variable declaration
+   int number=5;
+   print("final before->number:$number");
+  //  number=15;
+  //  print("final before->number:$number");//final
+
+  const number1=66;
+  print("const variable ->number1:$number1");//const
+  // number1=68;
+  // print("const variable ->number1:$number1");
+  bool marriedstatus=false;
+  print("my Marital status-> $marriedstatus");
+
+}
