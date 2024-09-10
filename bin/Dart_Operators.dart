09901bf87e20adb6e64cd837d927371985240549
GOHULRAJ <gohulrajs71@gmail.com>
2024-09-10 23:53:43 +0530

    Dart Operators

---
+++ bin/Dart_Operators.dart
@@ -0,0 +1,38 @@
+void main(){
+  var a=10,b=20;
+  /*Math Operators)*/
+  // print("$a + $b =${a+b}");
+  // print("$a - $b =${a-b}");
+  // print("$a * $b =${a*b}");
+  // print("$a / $b =${a/b}");
+
+  /*Conditional Operators*/
+  if(a>b){
+    print("a is less than b");
+  }
+  else{
+    print('b is grater then a');
+  }
+
+   if(a<b){
+    print("a is less than b");
+  }
+  else{
+    print('b is grater then a');
+  }
+
+   if(a==b){
+    print("a and b are equal");
+  }
+  else{
+    print('b not equal to a');
+  }
+
+   if(a!=b){
+    print("a and b are not equal");
+  }
+  else{
+    print('b and a are qual');
+  }
+
+}
