a8014940c16e89d2379673bd155a8fafd0b11c27
GOHULRAJ <gohulrajs71@gmail.com>
2024-09-11 09:46:12 +0530

    Increment & Decrement operator

---
+++ bin/Incr_Decr_Operator.dart
@@ -0,0 +1,12 @@
+void main() {
+  var a = 10;
+  print("a: $a");
+  print("a++: ${a++}");
+  print("After increament variable a: $a");
+  print("a++: ${a++}");
+  print("After increament variable a: $a");
+  print("a--: ${a--}");
+  print("After decrement variable a: $a");
+  print("a--: ${a--}");
+  print("After decrement variable a: $a");
+}
